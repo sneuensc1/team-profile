f9a28295fc3f23f2247a59ced1f4d4bd9238b686
saraneuie <sneuensc@umich.edu>
2022-04-06 20:11:09 -0400

    trying employee test failing run

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('..Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Mack');
+
+    expect(employee.name).toBe('Mack');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
