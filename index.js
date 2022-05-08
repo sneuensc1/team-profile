d65142f49758d291ea858c8e1436dd3ecd9d4353
saraneuie <sneuensc@umich.edu>
2022-05-07 23:01:08 -0400

    setting up more functions

--- index.js
+++ index.js
@@ -21,3 +21,28 @@ console.log('---------------');
 console.log('A Team Profile Generator by sneuensc1');
 
 //Other functions
+async function main () {
+    try {
+        await prompt ()
+
+        for (let i = 0; i < teamArray; i++) {
+            teamString = teamString + html.generateCard(teamArray[i]);
+        }
+        let completeHtml = html.generateHTML(teamString);
+
+        console.clear();
+        console.log('---------------');
+        console.log('Generating index.html file');
+        console.log('---------------');
+
+        writeFileAsync('./dist/index.html', completeHtml);
+
+        console.clear();
+        console.log('---------------');
+        console.log('File created successfully!');
+        console.log('---------------');
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
