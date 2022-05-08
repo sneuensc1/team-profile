5fcfb6bab3b1f960b921cf6edc2f6ea3bfbcfc71
saraneuie <sneuensc@umich.edu>
2022-05-07 20:51:26 -0400

    updating functions

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ const Intern = require('../Intern');
 const html = require('./src/htmlTemp');
 const email = require('email-validator');
 
+//asynchronous functions
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+let teamArray = [];
+let teamString = '';
+
+console.clear();
+console.log('---------------');
+console.log('A Team Profile Generator by sneuensc1');
+
+//Other functions
